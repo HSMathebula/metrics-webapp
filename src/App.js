af2a3de6913b59ecb0635a13c5692c966166a0d1
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-07-01 03:21:00 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailPage';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      {/* <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/country/:name" element={<DetailPage />} />
+      </Routes> */}
+    </Router>
   );
 }
 
